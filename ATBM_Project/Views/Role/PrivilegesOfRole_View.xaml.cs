f2f08f810f21382bfc2017d90a855fbc3f35fc89
ddhuu <ddhuu.dev@gmail.com>
2024-04-10 02:59:31 +0700

    Xong view của Role

---
+++ ATBM_Project/Views/Role/PrivilegesOfRole_View.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ATBM_Project.Views.Role
+{
+    /// <summary>
+    /// Interaction logic for PrivilegesOfRole.xaml
+    /// </summary>
+    public partial class PrivilegesOfRole_View : UserControl
+    {
+        public PrivilegesOfRole_View()
+        {
+            InitializeComponent();
+        }
+    }
+}
