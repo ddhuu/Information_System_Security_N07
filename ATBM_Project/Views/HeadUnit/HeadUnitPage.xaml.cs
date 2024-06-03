8d502a431e40e4039a32fe605ee1b7df71f740eb
ddhuu <ddhuu.dev@gmail.com>
2024-06-03 19:13:18 +0700

    Hanlde login for all Role

---
+++ ATBM_Project/Views/HeadUnit/HeadUnitPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System.Windows;
+
+namespace ATBM_Project.Views.HeadUnit
+{
+    /// <summary>
+    /// Interaction logic for HeadUnitPage.xaml
+    /// </summary>
+    /// 
+
+    public partial class HeadUnitPage : Window
+    {
+        private string _userName;
+        private OracleConnection _connection;
+        public HeadUnitPage(OracleConnection connection, string userName)
+        {
+            _userName = userName;
+            _connection = connection;
+            InitializeComponent();
+            Title.Text = userName;
+        }
+
+        public void clickBack(object sender, RoutedEventArgs e)
+        {
+            _connection.Close();
+            Login_View login = new Login_View();
+            this.Close();
+            login.Show();
+        }
+    }
+}
