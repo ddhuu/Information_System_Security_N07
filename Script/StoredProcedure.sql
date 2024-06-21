6e6dd57fd04254b755333a48b0d96bce7bea9f11
ddhuu <ddhuu.dev@gmail.com>
2024-06-21 10:49:25 +0700

    Add context

--- Script/StoredProcedure.sql
+++ Script/StoredProcedure.sql
@@ -49,6 +49,48 @@ BEGIN
   END LOOP;
 END;
 
+-- Create the context
+CREATE CONTEXT user_ctx USING user_ctx_pkg;
+
+-- Create the package
+CREATE OR REPLACE PACKAGE user_ctx_pkg IS
+  PROCEDURE set_role(p_role VARCHAR2);
+END;
+/
+
+-- Create the package body
+CREATE OR REPLACE PACKAGE BODY user_ctx_pkg IS
+  PROCEDURE set_role(p_role VARCHAR2) IS
+  BEGIN
+    DBMS_SESSION.SET_CONTEXT('user_ctx', 'role', p_role);
+  END;
+END;
+/
+
+-- Create the trigger
+CREATE OR REPLACE TRIGGER set_role_at_logon
+AFTER LOGON ON DATABASE
+DECLARE
+  v_role VARCHAR2(20);
+BEGIN
+  SELECT VAITRO INTO v_role FROM ADMIN.NHANSU WHERE MANV = USER;
+  IF v_role = 'TRUONG DON VI' THEN
+    user_ctx_pkg.set_role('RL_TRUONGDONVI');
+  ELSIF v_role = 'NHAN VIEN' THEN
+    user_ctx_pkg.set_role('RL_NHANVIENCOBAN');
+  ELSIF v_role = 'GIAO VU' THEN
+    user_ctx_pkg.set_role('RL_GIAOVU');
+  ELSIF v_role = 'GIANG VIEN' THEN
+    user_ctx_pkg.set_role('RL_GIANGVIEN');
+  ELSE
+    user_ctx_pkg.set_role('RL_SINHVIEN');
+  END IF;
+EXCEPTION
+  WHEN NO_DATA_FOUND THEN
+    user_ctx_pkg.set_role('RL_SINHVIEN');
+END;
+/
+
 
 
 -- Goi 2 thuc thi 2 procedure tren
