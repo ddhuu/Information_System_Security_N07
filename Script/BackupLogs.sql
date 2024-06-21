ca5667fb25550b2943c041e7ff8e5f84a8a162d9
ddhuu <ddhuu.dev@gmail.com>
2024-06-21 12:02:17 +0700

    remove index in tblBackupLogs

--- Script/BackupLogs.sql
+++ Script/BackupLogs.sql
@@ -2,6 +2,3 @@ CREATE TABLE tblBackupLog (
     BackupTimestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     BackupStatus VARCHAR(50)
 );
-
-CREATE INDEX idxBackupTimestamp ON tblBackupLog (BackupTimestamp);
-CREATE INDEX idxBackupStatus ON tblBackupLog (BackupStatus);
